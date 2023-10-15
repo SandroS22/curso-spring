c32704a9544b124fa6db0746966df6baee038e67
Sandro Santana <osandro@protonmail.com>
2023-10-15 17:48:47 -0300

    novo-projeto

---
+++ cspringboot/src/main/java/com/github/sandros22/springboot/cspringboot/CspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.github.sandros22.springboot.cspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CspringbootApplication.class, args);
+	}
+
+}
---
+++ cspringboot/src/test/java/com/github/sandros22/springboot/cspringboot/CspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.sandros22.springboot.cspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
